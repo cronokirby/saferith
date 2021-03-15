a3457543f10cc664e699a08b05362eb3e28d97b5
Lúcás Meier <cronokirby@gmail.com>
2021-03-15 19:26:26 +0100

    Play around with some benchmarks

--- num_bench_test.go
+++ num_bench_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 var resultInt big.Int
+var resultNat Nat
 
 func BenchmarkAddBig(b *testing.B) {
 	bytes := make([]byte, 100)
@@ -17,8 +18,53 @@ func BenchmarkAddBig(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		var z big.Int
 		z.Add(&x, &x)
-		z.Add(&x, &x)
-		z.Add(&x, &x)
 		resultInt = z
+	}
+}
+
+func BenchmarkAddBigMod(b *testing.B) {
+	bytes := make([]byte, 100)
+	for i := 0; i < 100; i++ {
+		bytes[i] = 1
+	}
+	var x big.Int
+	var m big.Int
+	x.SetBytes(bytes)
+	m.SetUint64(13)
+	for n := 0; n < b.N; n++ {
+		var z big.Int
+		z.Add(&x, &x)
+		z.Mod(&x, &m)
+		resultInt = z
+	}
+}
+
+func BenchmarkAddNat(b *testing.B) {
+	bytes := make([]byte, 100)
+	for i := 0; i < 100; i++ {
+		bytes[i] = 1
+	}
+	var x Nat
+	x.SetBytes(bytes)
+	for n := 0; n < b.N; n++ {
+		var z Nat
+		z.Add(&x, &x, 100*64)
+		resultNat = z
+	}
+}
+
+func BenchmarkAddNatMod(b *testing.B) {
+	bytes := make([]byte, 100)
+	for i := 0; i < 100; i++ {
+		bytes[i] = 1
+	}
+	var x Nat
+	var m Nat
+	x.SetBytes(bytes)
+	m.SetUint64(13)
+	for n := 0; n < b.N; n++ {
+		var z Nat
+		z.ModAdd(&x, &x, &m)
+		resultNat = z
 	}
 }
