910c84a417f0cc6130ba27e1dd1532832e333fe0
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-16 08:48:46 +0200

    Add limbMask function

--- bits.go
+++ bits.go
@@ -4,3 +4,16 @@ package safenum
 func limbCountFromBits(bits int) int {
 	return (bits + _W - 1) / _W
 }
+
+// limbMask returns the mask used for the final limb of a Nat with this number of bits.
+//
+// Note that this function will leak the number of bits. For our library, this isn't
+// a problem, since we always call this function with announced sizes.
+func limbMask(bits int) Word {
+	remaining := bits % _W
+	allOnes := ^Word(0)
+	if remaining == 0 {
+		return allOnes
+	}
+	return 1 ^ (allOnes << remaining)
+}
---
+++ bits_test.go
@@ -0,0 +1,15 @@
+package safenum
+
+import (
+	"math/rand"
+	"testing"
+)
+
+var result Word
+
+func BenchmarkLimbMask(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		x := rand.Int()
+		result = limbMask(x)
+	}
+}
