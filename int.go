2b5a143fd0f4d5c54e1ed9afbcb95b5beb48c32c
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-16 14:46:05 +0200

    Implement multiplication for Int

--- int.go
+++ int.go
@@ -28,6 +28,15 @@ func (z *Int) SetBytes(data []byte) *Int {
 	return z
 }
 
+// SetUint64 sets the value of z to x.
+//
+// This number will be positive.
+func (z *Int) SetUint64(x uint64) *Int {
+	z.sign = 0
+	z.abs.SetUint64(x)
+	return z
+}
+
 // String formats this number as a signed hex string.
 //
 // This isn't a format that Int knows how to parse. This function exists mainly
@@ -54,5 +63,17 @@ func (z *Int) Eq(x *Int) Choice {
 func (z *Int) Neg(x *Int) *Int {
 	z.sign = 1 ^ x.sign
 	z.abs.SetNat(&x.abs)
+	return z
+}
+
+// Mul calculates z <- x * y, returning z.
+//
+// This will truncate the resulting absolute value, based on the bit capacity passed in.
+//
+// If cap < 0, then capacity is x.AnnouncedLen() + y.AnnouncedLen().
+func (z *Int) Mul(x *Int, y *Int, cap int) *Int {
+	// (-1)^sx * ax * (-1)^sy * ay = (-1)^(sx + sy) * ax * ay
+	z.sign = x.sign ^ y.sign
+	z.abs.Mul(&x.abs, &y.abs, cap)
 	return z
 }
