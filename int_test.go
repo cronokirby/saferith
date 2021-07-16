85e1e71222afe56cf65c5f7f666fe1e05b4a3417
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-16 14:46:05 +0200

    Implement multiplication for Int

--- int_test.go
+++ int_test.go
@@ -12,7 +12,7 @@ func (*Int) Generate(r *rand.Rand, size int) reflect.Value {
 	r.Read(bytes)
 	i := new(Int).SetBytes(bytes)
 	if r.Int()&1 == 1 {
-		i.Neg()
+		i.Neg(i)
 	}
 	return reflect.ValueOf(i)
 }
@@ -23,6 +23,48 @@ func testIntEqualReflexive(z *Int) bool {
 
 func TestIntEqualReflexive(t *testing.T) {
 	err := quick.Check(testIntEqualReflexive, &quick.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testIntMulCommutative(x, y *Int) bool {
+	way1 := new(Int).Mul(x, y, -1)
+	way2 := new(Int).Mul(y, x, -1)
+	return way1.Eq(way2) == 1
+}
+
+func TestIntMulCommutative(t *testing.T) {
+	err := quick.Check(testIntMulCommutative, &quick.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testIntMulZeroIsZero(x *Int) bool {
+	zero := new(Int)
+	timesZero := new(Int).Mul(zero, x, -1)
+	return timesZero.Eq(zero) == 1
+}
+
+func TestIntMulZeroIsZero(t *testing.T) {
+	err := quick.Check(testIntMulZeroIsZero, &quick.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testIntMulNegativeOneIsNeg(x *Int) bool {
+	minusOne := new(Int).SetUint64(1)
+	minusOne.Neg(minusOne)
+
+	way1 := new(Int).Neg(x)
+	way2 := new(Int).Mul(x, minusOne, -1)
+	return way1.Eq(way2) == 1
+}
+
+func TestIntMulNegativeOneIsNeg(t *testing.T) {
+	err := quick.Check(testIntMulNegativeOneIsNeg, &quick.Config{})
 	if err != nil {
 		t.Error(err)
 	}
