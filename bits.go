f9715bac2b6af0d6da55fb81d9257eea6f4eeac8
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-17 11:12:20 +0200

    Make bits use more constants

--- bits.go
+++ bits.go
@@ -1,8 +1,14 @@
 package safenum
 
+// _WShift can be used to multiply or divide by _W
+//
+// This assumes that _W = 64, 32
+const _WShift = 4 | ((_W >> 6) << 1)
+const _WMask = _W - 1
+
 // limbCount returns the number of limbs needed to accomodate bits.
 func limbCount(bits int) int {
-	return (bits + _W - 1) / _W
+	return (bits + _W - 1) >> _WShift
 }
 
 // limbMask returns the mask used for the final limb of a Nat with this number of bits.
@@ -10,7 +16,7 @@ func limbCount(bits int) int {
 // Note that this function will leak the number of bits. For our library, this isn't
 // a problem, since we always call this function with announced sizes.
 func limbMask(bits int) Word {
-	remaining := bits % _W
+	remaining := bits & _WMask
 	allOnes := ^Word(0)
 	if remaining == 0 {
 		return allOnes
